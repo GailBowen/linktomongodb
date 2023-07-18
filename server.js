bb7d7c431379082bf9b4a2409b782738f5ece3cd
Gail Foad <gail.foad@thenavpeople.co.uk>
2023-07-18 09:42:24 +0100

    Save to mongodb

---
+++ server.js
@@ -0,0 +1,27 @@
+console.log('Gail - hello from server.js');
+const express = require('express');
+const mongoose = require('mongoose');
+const Article = require('./articles');
+const app = express();
+//const methodOverride = require('method-override');
+
+mongoose.connect('mongodb://127.0.0.1/fetaomelette', {useNewUrlParser: true, useUnifiedTopology: true});
+
+console.log('am I here?');
+
+let article = new Article({ 
+        title: 'feta',
+        description: 'feta omelette',
+        markdown: 'I love Terry'
+    });
+
+    try {
+        article = article.save();
+     }
+     catch (e) {
+         console.log(e);
+     }
+
+     app.use(express.urlencoded({extended: false}));
+
+     app.listen(5003);
